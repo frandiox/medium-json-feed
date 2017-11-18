4612d264b48947b52029dd44ac3876a97d5b78cf
Fran Dios <frankdiox@gmail.com>
2017-11-18 23:36:39 +0900

    Add minimum server.

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const mediumJSONFeed = require('./index.js');
+
+const port = process.env.PORT || 3000;
+const respond = (res, data = {}) => {
+  if (!res.finished) {
+    res.writeHead(data.status || 500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(data, null, 2), 'utf-8');
+  }
+};
+
+http.createServer((req, res) => {
+  if (req.method !== 'GET' || ['/robots.txt', '/favicon.ico'].indexOf(req.url) !== -1) {
+    res.writeHead(204);
+    return res.end();
+  }
+
+  console.log(`> GET: '${req.url}' --- ${new Date()}`);
+
+  mediumJSONFeed(req.url)
+    .then(data => respond(res, data))
+    .catch(error => respond(res, error));
+
+}).listen(port);
+
+console.info('>>> Server listening on port', port, '\n');
